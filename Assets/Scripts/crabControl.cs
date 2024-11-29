cc7dceab25b59de9e347acd519333595a80cf875
Mitus-22 <eortare690@g.educaand.es>
2024-11-29 21:06:21 +0100

    29/11
    Con los assets de clase, animaciones y disparo (hay que arreglar el disparo)

---
+++ Assets/Scripts/crabControl.cs
@@ -0,0 +1,59 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class crabControl : MonoBehaviour
+{
+    [SerializeField] int speed = 3;
+    [SerializeField] Vector3 endPosition;  
+    Vector3 startPosition;
+    bool goingToTheEnd = true; 
+
+    [SerializeField] SpriteRenderer sprite;
+    float previousXPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+        previousXPos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goingToTheEnd){
+            transform.position = Vector3.MoveTowards(
+                           transform.position,
+                           endPosition,
+                           speed * Time.deltaTime);
+            if (transform.position == endPosition){
+                goingToTheEnd = false; 
+            }
+        } else {
+            transform.position = Vector3.MoveTowards(
+                           transform.position,
+                           startPosition,
+                           speed * Time.deltaTime);
+            if (transform.position == startPosition){
+                goingToTheEnd = true;       
+            }
+        }
+
+        if (transform.position.x > previousXPos){
+            sprite.flipX = true;
+        } else if (transform.position.x < previousXPos){
+            sprite.flipX = false;
+        }
+
+        previousXPos = transform.position.x;
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+      if (other.gameObject.tag == "Player"){
+        if (GameManager.invulnerable == false){
+            other.gameObject.SendMessage("damage");
+        }
+      }  
+    }
+}
---
+++ Assets/Scripts/playerControl1.cs
@@ -0,0 +1,97 @@
+using System.Linq.Expressions;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerControl1 : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public int speed = 4;
+    public int jump = 5;
+
+    [SerializeField] SpriteRenderer sprite;
+    [SerializeField] Animator anim;
+    [SerializeField] int lives = 3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(inputX * speed, rb.linearVelocity.y);
+
+        if (inputX > 0){ //Derecha
+           sprite.flipX = false; 
+        } else if (inputX < 0){  //Izquierda
+           sprite.flipX = true;
+        }
+
+        //Animaciones
+        if (Input.GetKey(KeyCode.A) || 
+            Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.RightArrow)){
+              anim.SetBool("isRunning",true);
+        } else {
+              anim.SetBool("isRunning",false);
+        }
+ 
+        if (grounded()==false){
+            anim.SetBool("isJumping",true);
+        } else {
+            anim.SetBool("isJumping",false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded()){
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+    }
+
+    bool grounded(){
+        RaycastHit2D touch = Physics2D.Raycast(transform.position,
+                                              Vector2.down,
+                                              0.2f); 
+        if (touch.collider ==  null){
+            return false; 
+        } else {
+            return true;
+        }                                      
+    }
+
+   void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag == "PowerUp"){
+            Destroy(other.gameObject);
+            sprite.color = Color.yellow;
+            GameManager.invulnerable = true;
+            Invoke("becomeVulnerable",3);
+
+        }
+   }
+
+   void becomeVulnerable(){
+        sprite.color = Color.white;
+        GameManager.invulnerable = false;
+   }
+
+   public void damage(){
+        lives--;
+        sprite.color = Color.red;
+        GameManager.invulnerable = true;
+        Invoke("becomeVulnerable",1);
+        if (lives < 0){
+            Destroy(gameObject);
+            SceneManager.LoadScene("Level1");
+        }
+   }
+
+    //Disparo
+    if (Input.GetMouseButtonDown(0)){
+        Instantiate(Shot, New Vector3(transform.position.x, transform.position.y, + 1,7f), Quaternion.identity);
+    }
+
+}
